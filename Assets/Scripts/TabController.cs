d38bf1fd445fb9fb465b0ed0aba6ab9a1f7e6501
Thereza Schmelter <s59091@beuth-hochschule.de>
2017-06-18 05:10:58 +0200

    Add tab indicator

--- Assets/Scripts/TabController.cs
+++ Assets/Scripts/TabController.cs
@@ -6,22 +6,26 @@ using UnityEngine.UI;
 public class TabController : MonoBehaviour {
 
     public RectTransform content;
+    public RectTransform indicator;
 
     public void OnClickPuzzle()
     {
         Debug.Log("Puzzle Tab");
         content.anchoredPosition = new Vector2(0, 860);
+        indicator.anchoredPosition = new Vector2(-255, -90);
     }
 
     public void OnClickStats()
     {
         Debug.Log("Stats Tab");
         content.anchoredPosition = new Vector2(-1080, 860);
+        indicator.anchoredPosition = new Vector2(38, -90);
     }
 
     public void OnClickRewards()
     {
         Debug.Log("Rewards Tab");
         content.anchoredPosition = new Vector2(-2160, 860);
+        indicator.anchoredPosition = new Vector2(341, -90);
     }
 }
