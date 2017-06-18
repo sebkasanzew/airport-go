1a273cbaf8941abba4175ea4123277867a23f6ca
Thereza Schmelter <s59091@beuth-hochschule.de>
2017-06-18 04:50:34 +0200

    Ad QR code stuff

---
+++ Assets/Scripts/RewardController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardController : MonoBehaviour {
+    public GameObject qrView;
+
+    public void ShowQRCode(int points)
+    {
+        if (points <= ApplicationManager.Instance.points)
+        {
+            ApplicationManager.Instance.AddPoints(-points);
+            qrView.SetActive(true);
+            ApplicationManager.Instance.AddListenerToBackButton(HideQRCode);
+        }
+        
+    }
+
+    public void HideQRCode()
+    {
+        ApplicationManager.Instance.HideBackbutton();
+        qrView.SetActive(false);
+    }
+}
